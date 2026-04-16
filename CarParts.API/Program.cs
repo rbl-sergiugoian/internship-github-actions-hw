627917abd152ad6986680de8026f2828acb1e6bd
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-16 16:47:37 +0300

    ci-cd: removed commented jwt auth code from program cs, added versioning and taggging to the deploy pipeline

--- CarParts.API/Program.cs
+++ CarParts.API/Program.cs
@@ -11,84 +11,13 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.Configure<AuthorizationSettings>(
-//    builder.Configuration.GetSection("AuthorizationSettings"));
-
-//var authSettings = builder.Configuration
-//    .GetSection("AuthorizationSettings")
-//    .Get<AuthorizationSettings>();
-
 builder.Services.AddSingleton<ICarPartsRepository, CarPartsRepository>();
 builder.Services.AddScoped<ICarPartsService, CarPartsService>();
-//builder.Services.AddSingleton<IUserService, UserService>();
-//builder.Services.AddSingleton<IJwtUtils, JwtUtils>();
 
 builder.Services.AddControllers();
 
-
-//builder.Services.AddProblemDetails();
-
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//            .AddJwtBearer(options =>
-//            {
-//                options.RequireHttpsMetadata = false;
-//                options.SaveToken = true;
-//                options.MapInboundClaims = false;
-//                options.TokenValidationParameters = new TokenValidationParameters
-//                {
-//                    ValidateIssuer = true,
-//                    ValidateAudience = true,
-//                    ValidateLifetime = true,
-//                    ValidateIssuerSigningKey = true,
-//                    ValidIssuer = authSettings.Issuer,
-//                    ValidAudience = authSettings.Audience,
-//                    IssuerSigningKey = new SymmetricSecurityKey(
-//                        Encoding.UTF8.GetBytes(authSettings.Secret)
-//                    ),
-//                    ClockSkew = TimeSpan.Zero
-//                };
-//            });
-
-
-//builder.Services.AddAuthorization(config =>
-//{
-//    config.AddPolicy(Policies.User, policy =>
-//        policy.Requirements.Add(new PrivilegeRequirement(Policies.User)));
-
-//    config.AddPolicy(Policies.Admin, policy =>
-//        policy.Requirements.Add(new PrivilegeRequirement(Policies.Admin)));
-
-//    config.AddPolicy(Policies.All, policy =>
-//        policy.Requirements.Add(new PrivilegeRequirement(Policies.All)));
-//});
-
-//builder.Services.AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    //options.SwaggerDoc("v1", new OpenApiInfo
-    //{
-    //    Version = "v1",
-    //    Title = "Car Parts App",
-    //    Description = "Simple web API that helps usrs manage their cae parts",
-    //});
-
-    //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    //{
-    //    Name = "Authorization",
-    //    Type = SecuritySchemeType.Http,
-    //    Scheme = "bearer",
-    //    BearerFormat = "JWT",
-    //    In = ParameterLocation.Header,
-    //    Description = "Enter only the JWT token"
-    //});
-
-    //options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-    //{
-    //    [new OpenApiSecuritySchemeReference("Bearer", document)] = []
-    //});
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -103,14 +32,9 @@ app.UseSwaggerUI(options =>
 
 if (app.Environment.IsDevelopment())
 {
-    //app.UseExceptionHandler();
 }
 
 app.UseHttpsRedirection();
-
-//app.UseAuthentication();
-//app.UseAuthorization();
-//app.UseMiddleware<JwtMiddleware>();
 
 app.MapControllers();
 
